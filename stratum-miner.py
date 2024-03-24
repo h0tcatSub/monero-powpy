a8fe56dff3366e4680a357e7be1be00f7ad7a598
= <n0ricatnohotmens@gmail.com>
2024-03-24 20:53:10 +0900

    test commit

--- stratum-miner.py
+++ stratum-miner.py
@@ -117,7 +117,7 @@ get_hash = np.vectorize(get_hash)
 
 def get_r64(hash):
     #r64 = struct.unpack('Q', hash[24:])[0]
-    print(len(hash[24]))
+    print((hash[24:]))
     r64 = struct.unpack('Q', hash[24:])[0]
     return r64
 get_r64 = np.vectorize(get_r64)
