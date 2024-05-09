7869ffc412be050c6a7307b45de2d4360ac0e93f
加藤嵩大 <takahiro@katoushiori-no-MacBook-Air.local>
2024-05-09 19:36:03 +0900

    TEST

--- stratum_miner.py
+++ stratum_miner.py
@@ -142,6 +142,7 @@ def worker(q, s):
                 print("FOUND NONCE!!")
                 print('{}Hashrate: {} H/s'.format(os.linesep, hr))
                 r64 = map(create_r64, hash)
+                print("Creating R64...")
                 r64 = list(r64)
                 hex_hash = list(hex_hash)
                 found_nonce_index = np.where(r64  < target)[0][0]
