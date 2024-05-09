b4d80394e5551cadf39fa9c80bd1bd2bf3386675
h0tcatSub <n0ricatnohotmens@gmail.com>
2024-05-09 18:49:16 +0900

    TEST

--- stratum_miner.py
+++ stratum_miner.py
@@ -1,38 +1,8 @@
-#  Copyright (c) 2019, The Monero Project
-#  
-#  All rights reserved.
-#  
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are met:
-#  
-#  1. Redistributions of source code must retain the above copyright notice, this
-#  list of conditions and the following disclaimer.
-#  
-#  2. Redistributions in binary form must reproduce the above copyright notice,
-#  this list of conditions and the following disclaimer in the documentation
-#  and/or other materials provided with the distribution.
-#  
-#  3. Neither the name of the copyright holder nor the names of its contributors
-#  may be used to endorse or promote products derived from this software without
-#  specific prior written permission.
-#  
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-#  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 import argparse
 import socket
 import select
 import binascii
 import numpy as np  
-import pycryptonight
 
 import pyrx
 import struct
@@ -46,7 +16,7 @@ from multiprocessing import Process, Queue
 pool_host = 'monerop.com'
 pool_port = 4242
 pool_pass = 'xx'
-wallet_address = '42ydzcV2cJxGzKpmZMbsmtahZUhprCspgP9pBYoDk5F5HGUYTCBY3Sn83zTcm5yYeVPYsvikWim35ZajZqo5ReBPFg2CF79'
+wallet_address = ''
 nicehash = False
 
 
@@ -150,8 +120,8 @@ def worker(q, s):
             bins = map(pack_nonce, blob, nonces)
             if cnv > 5:
                 hash = map(pyrx.get_rx_hash, bins, seed_hash, height)
-            else:
-                hash = map(pycryptonight.cn_slow_hash, bins, cnv, 0, height)
+            #else:
+            #    hash = map(pycryptonight.cn_slow_hash, bins, cnv, 0, height)
             hash_count += range_bits
             sys.stdout.write('.')
             sys.stdout.flush()
@@ -192,6 +162,9 @@ if __name__ == '__main__':
     parser.add_argument('--nicehash', action='store_true', help='NiceHash mode')
     parser.add_argument('--host', action='store', help='Pool host')
     parser.add_argument('--port', action='store', help='Pool port')
+
+    parser.add_argument('--user', action='store', help='Miner Address')
+    parser.add_argument('--password', action='store', help='Mining password')
     args = parser.parse_args()
     if args.nicehash:
         nicehash = True
@@ -199,4 +172,6 @@ if __name__ == '__main__':
         pool_host = args.host
     if args.port:
         pool_port = int(args.port)
+    wallet_address = args.user
+    pool_pass = args.password
     main()
